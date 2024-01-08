9deac4c22d6b756b6fbcf3ed294e164808da7200
MUKONGE <mukonge.eric@gmail.com>
2024-01-08 13:19:10 +0300

    Update

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -10,5 +11,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///siteitems.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
+Login_manager = LoginManager(app)
 
 from flaskblog import routes 
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 
 
-from flaskblog import app,db
+from flaskblog import app, db
 
 if __name__ == "__main__":
     app.run(debug=True) 
