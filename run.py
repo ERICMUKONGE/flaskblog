57a7e1aecad2d03a88913ba3ec682536c383b122
MUKONGE <mukonge.eric@gmail.com>
2024-01-11 20:00:28 +0300

    Update

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-from flaskblog import app
+from flaskblog import app, db
 
 if __name__ == "__main__":
     app.run(debug=True) 
 
-# @app.cli.command('db_create')
-# def db_create():
-#     with app.app_context():
-#         db.create_all()
-#         print('Database created') 
+@app.cli.command('db_create')
+def db_create():
+    with app.app_context():
+        db.create_all()
+        print('Database created') 
